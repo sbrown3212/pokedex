74c223b1cfbe0f9e5b8eaec43071a1aad29e3558
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-11-12 15:06:59 -0700

    Add initial repl logic.

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+)
+
+const (
+	prompt string = "Pokedex > "
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print(prompt)
+		scanner.Scan()
+		text := scanner.Text()
+		cleaned := cleanInput(text)
+		fmt.Printf("Your command was: %v\n", cleaned[0])
+	}
 }
