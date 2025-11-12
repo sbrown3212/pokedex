28c76fc5841e0e58b2ab8170a02fc4fdab70718f
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-11-12 13:51:40 -0700

    Add 'cleanInput' func and tests.

--- repl.go
+++ repl.go
@@ -1,6 +1,15 @@
 package main
 
+import (
+	"strings"
+)
+
 func cleanInput(text string) []string {
 	var result []string
+
+	trimmed := strings.TrimSpace(text)
+	lowercase := strings.ToLower(trimmed)
+	result = strings.Split(lowercase, " ")
+
 	return result
 }
---
+++ repl_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := map[string]struct {
+		input    string
+		expected []string
+	}{
+		"split_on_whitespace": {
+			input:    "hello world",
+			expected: []string{"hello", "world"},
+		},
+		"strip_whitespace": {
+			input:    "  hello world  ",
+			expected: []string{"hello", "world"},
+		},
+		"lowercase": {
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			actual := cleanInput(tc.input)
+			if len(actual) != len(tc.expected) {
+				t.Errorf("expected: %v, actual: %v", len(tc.expected), len(actual))
+			}
+			for i := range actual {
+				word := actual[i]
+				expectedWord := tc.expected[i]
+				if word != expectedWord {
+					t.Errorf("Index: %v, expected word: %v, actual word: %v", i, word, expectedWord)
+				}
+			}
+		})
+	}
+}
