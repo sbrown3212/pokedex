512c8f1260778fe5f0a34ee8a0b9c937cacdc891
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-11-12 15:12:12 -0700

    Extracted repl logic to repl.go.

--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-const (
-	prompt string = "Pokedex > "
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print(prompt)
-		scanner.Scan()
-		text := scanner.Text()
-		cleaned := cleanInput(text)
-		fmt.Printf("Your command was: %v\n", cleaned[0])
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
+)
+
+const (
+	prompt string = "Pokedex > "
 )
 
 func cleanInput(text string) []string {
@@ -9,4 +16,21 @@ func cleanInput(text string) []string {
 	words := strings.Fields(lowercase)
 
 	return words
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print(prompt)
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		fmt.Printf("Your command was: %v\n", commandName)
+	}
 }
