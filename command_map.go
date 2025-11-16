1f586d4b6c0abf99ec7942793e559981b703e244
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-11-16 15:31:11 -0700

    Refactored 'map' command functions to use pokeapi package.

--- command_map.go
+++ command_map.go
@@ -1,65 +1,39 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 )
 
-func commandMap(cfg *config) error {
-	const locationAreaURL string = "https://pokeapi.co/api/v2/location-area"
-
-	// Initialize config
-	if cfg.Next == "" && cfg.Previous == "" {
-		cfg.Next = locationAreaURL
-	}
-
-	if cfg.Next == "" {
-		fmt.Println("You've reached the end of the list")
-		return nil
-	}
-
-	res, err := http.Get(cfg.Next)
+func commandMapf(cfg *config) error {
+	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.nextLocationsURL)
 	if err != nil {
-		return fmt.Errorf("location area GET request error: %s", err)
-	}
-	defer res.Body.Close()
-
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return fmt.Errorf("location area io ReadAll error: %s", err)
+		return err
 	}
 
-	var locations locationAreaResponse
-	if err := json.Unmarshal(data, &locations); err != nil {
-		return fmt.Errorf("location area unmarshal error: %s", err)
-	}
+	cfg.nextLocationsURL = locationsResp.Next
+	cfg.prevLocationsURL = locationsResp.Previous
 
-	if locations.Next == nil {
-		cfg.Next = ""
-	} else {
-		cfg.Next = *locations.Next
-	}
-	if locations.Previous == nil {
-		cfg.Previous = ""
-	} else {
-		cfg.Previous = *locations.Previous
-	}
-
-	for _, location := range locations.Results {
+	for _, location := range locationsResp.Results {
 		fmt.Println(location.Name)
 	}
-
 	return nil
 }
 
-type locationAreaResponse struct {
-	Count    int     `json:"count"`
-	Next     *string `json:"next"`
-	Previous *string `json:"previous"`
-	Results  []struct {
-		Name string `json:"name"`
-		URL  string `json:"url"`
-	} `json:"results"`
+func commandMapB(cfg *config) error {
+	if cfg.prevLocationsURL == nil {
+		return fmt.Errorf("you're at the beginnning of the list")
+	}
+
+	locationsResp, err := cfg.pokeapiClient.ListLocations(cfg.prevLocationsURL)
+	if err != nil {
+		return err
+	}
+
+	cfg.nextLocationsURL = locationsResp.Next
+	cfg.prevLocationsURL = locationsResp.Previous
+
+	for _, location := range locationsResp.Results {
+		fmt.Println(location.Name)
+	}
+	return nil
 }
