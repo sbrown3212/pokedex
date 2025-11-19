0a392116ec9c63d49b719351db004ffe5b0f358b
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-11-18 21:37:58 -0700

    Added reapLoop test.

--- internal/pokecache/pokecache_test.go
+++ internal/pokecache/pokecache_test.go
@@ -37,3 +37,23 @@ func TestAddGet(t *testing.T) {
 		})
 	}
 }
+
+func TestReapLoop(t *testing.T) {
+	const baseTime = 5 * time.Millisecond
+	const waitTime = baseTime + 5*time.Millisecond
+	cache := NewCache(baseTime)
+	cache.Add("https://example.com", []byte("testdata"))
+
+	_, ok := cache.Get("https://example.com")
+	if !ok {
+		t.Errorf("expected to find key")
+		return
+	}
+
+	time.Sleep(waitTime)
+	_, ok = cache.Get("https://example.com")
+	if ok {
+		t.Errorf("expected to not find key")
+		return
+	}
+}
