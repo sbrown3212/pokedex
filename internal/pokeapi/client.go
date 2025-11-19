06f0d63a1cf785812fa1212e73763f5b5e8c1fed
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-11-18 20:52:21 -0700

    pokeapi package now uses pokecache.

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,16 +3,21 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/sbrown3212/pokedex/internal/pokecache"
 )
 
 type Client struct {
 	httpClient http.Client
+	cache      *pokecache.Cache
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(timeout time.Duration, interval time.Duration) Client {
+	cache := pokecache.NewCache(interval)
 	return Client{
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
+		cache: cache,
 	}
 }
--- internal/pokeapi/locations_list.go
+++ internal/pokeapi/locations_list.go
@@ -12,6 +12,16 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		url = *pageURL
 	}
 
+	cacheVal, ok := c.cache.Get(url)
+	if ok {
+		cachedLocations := RespShallowLocations{}
+		if err := json.Unmarshal(cacheVal, &cachedLocations); err != nil {
+			return RespShallowLocations{}, err
+		}
+
+		return cachedLocations, nil
+	}
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return RespShallowLocations{}, err
@@ -32,6 +42,8 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 	if err = json.Unmarshal(data, &locationsResp); err != nil {
 		return RespShallowLocations{}, err
 	}
+
+	c.cache.Add(url, data)
 
 	return locationsResp, nil
 }
